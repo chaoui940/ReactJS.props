02e4461fff80ab0fc77994c4a6c2fe798d9e9291
chaoui940 <daging129@gmail.com>
2022-02-05 21:44:21 +0100

    *

--- props/src/components/message.js
+++ props/src/components/message.js
@@ -1,10 +1,27 @@
 import React ,{ Component } from 'react'
 
 class Message extends Component {
+    constructor() {
+        super()
+        this.state ={
+            message : 'Welcome visitor'
+        }
+    }
+    changeMessage (){
+        this.setState( 
+            {
+                message :'Thank you for your subscribing'
+            }
+        )
+    }
     render()  {
         return (
-            <h1> Weclome Visitor
+            <div>
+                <h1> {this.state.message}
             </h1>
+            <button onClick={()=> this.changeMessage()}>Subscribe</button>
+            </div>
+            
         )
         
                    
