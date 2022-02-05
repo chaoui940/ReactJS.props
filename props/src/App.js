1604aa0bac3e10c1a9ad69effaf02ef4f44dd585
chaoui940 <daging129@gmail.com>
2022-02-05 21:25:20 +0100

    *

--- props/src/App.js
+++ props/src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 import Welcome from './components/welcome';
+import Message from './components/message';
 class App extends Component {
 render () {
   return (
     <div className="App" >
+      <Message />
       <Hello name="Simou" id="Dudu" > this is a children </Hello>
       <Hello name="ibt" id="Dudu">
         <button>click..</button>
--- props/src/components/message.js
+++ props/src/components/message.js
@@ -3,7 +3,7 @@ import React ,{ Component } from 'react'
 class Message extends Component {
     render()  {
         return (
-            <h1> tihs is a class component  a.k.a {this.props.id}
+            <h1> Weclome Visitor
             </h1>
         )
         
